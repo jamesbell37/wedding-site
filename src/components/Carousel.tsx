215b6295d0e3861aa0285b129f0a5f9dafcb8764
jamesb-demandlocal <93282298+jamesb-demandlocal@users.noreply.github.com>
2024-01-31 23:21:36 +0100

    fix image jumpiness

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -49,6 +49,7 @@ const Carousel = () => {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.5 }}
+          loading="lazy"
         />
       </AnimatePresence>
       {/* <button
