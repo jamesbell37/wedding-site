313f32a2e0e2b7c0b493e74900c46a7bcca2f580
jamesb-demandlocal <93282298+jamesb-demandlocal@users.noreply.github.com>
2024-02-19 14:19:29 +0100

    taxi prefix

--- src/pages/Taxis.tsx
+++ src/pages/Taxis.tsx
@@ -1,17 +1,18 @@
 import { useTranslation } from "react-i18next";
 
 const Taxis = ({ isMenuOpen }: { isMenuOpen: boolean }) => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const prefix = i18n.language === "en" ? "+34 " : "";
   return (
     !isMenuOpen && (
       <div className="flex flex-col flex-1 gap-5 px-5 bg-off-white h-screen">
         <div className="text-xl font-bold">Taxis</div>
         <div className={"text-md"}>{t("taxis.title")}</div>
         <ul className="list-disc px-5">
-          <li>Pide Taxi Cantabria (942333333)</li>
-          <li>Taxis Torrelavega (942888888)</li>
-          <li>Taxis Santander (942343434)</li>
-          <li>Taxis Somo/Langre (669557476)</li>
+          <li>Pide Taxi Cantabria ({prefix}942333333)</li>
+          <li>Taxis Torrelavega ({prefix}942888888)</li>
+          <li>Taxis Santander ({prefix}942343434)</li>
+          <li>Taxis Somo/Langre ({prefix}669557476)</li>
         </ul>
       </div>
     )
