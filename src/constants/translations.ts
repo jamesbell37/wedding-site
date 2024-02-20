d6ef0686877b31580017b6f863a019ed66793f9a
jamesb-demandlocal <93282298+jamesb-demandlocal@users.noreply.github.com>
2024-02-20 16:08:37 +0100

    fix spacing

--- src/constants/translations.ts
+++ src/constants/translations.ts
@@ -21,8 +21,10 @@ const resources = {
         <strong>Date</strong>: Friday July 5th, 2024.
         <strong>Time and Place</strong>: 5:00pm in Iglesia de Nuestra Señora de la Asunción (calle verdes 145, 39160, Galizano, Cantabria).
         <strong>Reception</strong>: Finca de San Juan Hostería & Catering (Barrio de San Juan, s/n, 39660 Villabanez, Cantabria).
+
         While we wait for this special day, we've created this site to provide all the info you may need.
         We will appreciate if you could please RSVP as soon as possible!
+        
         See you soon!
         Cristina & Ricardo`,
         hotel_rec: "Our hotel recommendations",
@@ -89,10 +91,12 @@ const resources = {
         body: "¡Estamos super felices y nos hace mucha ilusión que nos acompañéis en un día tan especial!",
         info: `
         <strong>Fecha</strong>: viernes 5 de julio de 2024.
-        <strong>Hora y lugar de la ceremonia</strong>: 17:00pm en la Iglesia de Nuestra Señora de la Asunción (calle verdes 145, 39160, Galizano, Cantabria).
-        <strong>Lugar de celebración</strong>: Finca de San Juan Hostería & Catering (Barrio de San Juan, s/n, 39660 Villabanez, Cantabria).
+        <strong>Hora y lugar de la ceremonia</strong>: 17:00pm en la Iglesia de Nuestra Señora de la Asunción (<a style="text-decoration:underline;" rel="noreferrer" target="_blank" href="https://maps.app.goo.gl/moae4YPy4ybuS2tb7">calle verdes 145, 39160, Galizano, Cantabria</a>).
+        <strong>Lugar de celebración</strong>: Finca de San Juan Hostería & Catering (<a style="text-decoration:underline;" rel="noreferrer" target="_blank" href="https://maps.app.goo.gl/ASTmheeSVEdpWMuu8">Barrio de San Juan, s/n, 39660 Villabanez, Cantabria</a>).
+        
         Mientras llega el gran día, hemos creado esta página web para que podáis tener toda la información que necesitáis.
         Confirmadnos lo antes posible por favor, que así organizarlo todo nos será mucho más fácil.
+        
         ¡Nos vemos muy pronto!
         Cristina & Ricardo`,
         hotel_rec: "Nuestras recomendaciones de alojamiento",
