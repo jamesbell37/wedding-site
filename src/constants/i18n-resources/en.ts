debc5bca4b1145bb3f6558d59f6369c56de1bb84
jamesb-demandlocal <93282298+jamesb-demandlocal@users.noreply.github.com>
2024-02-22 15:18:25 +0100

    refactor i18n

---
+++ src/constants/i18n-resources/en.ts
@@ -0,0 +1,76 @@
+export default {
+  translation: {
+    common: {
+      view: "view",
+      rsvp: "Click to RSVP",
+    },
+    menu: {
+      home: "Info",
+      ceremony: "Ceremony",
+      celebration: "Celebration",
+      hotels: "Hotels",
+      buses: "Buses",
+      taxis: "Taxis",
+      gifts: "Gifts",
+      contact: "Contact",
+    },
+    home: {
+      title: "We're getting married!",
+      body: "We are extremely excited for you to join us on this special day!",
+      info: `
+        <strong>Date</strong>: Friday July 5th, 2024.
+        <strong>Time and Place</strong>: 5:00pm in Iglesia de Nuestra Señora de la Asunción (calle verdes 145, 39160, Galizano, Cantabria).
+        <strong>Reception</strong>: Finca de San Juan Hostería & Catering (Barrio de San Juan, s/n, 39660 Villabanez, Cantabria).
+
+        While we wait for this special day, Cristina's amazing brother James created this site to provide all the info you may need.
+        We will appreciate if you could please RSVP as soon as possible!
+        
+        See you soon!
+        Cristina & Ricardo`,
+      hotel_rec: "Our hotel recommendations",
+    },
+    church: {
+      title: `Church`,
+      location: "Location",
+    },
+    celebration: {
+      title: `Reception`,
+      location: "Location",
+    },
+    hotels: {
+      title: "Hotels",
+      zone1: "Zone 1: Near the Celebration",
+      zone2: "Zone 2: In the City",
+      zone3: "Zone 3: Near the Church",
+    },
+    buses: {
+      title: "Buses",
+      body: `
+        We will offer a bus transport service that will take you to the ceremony in Iglesia de Nuestra Señora de la Asunción, and then to the reception at Finca de San Juan. Once the celebration is finished, there will be bus service to return.
+
+        In the coming weeks we will publish the bus stops and times.`,
+    },
+    taxis: {
+      title:
+        "We have compiled a list of taxi services in the area for your convenience",
+    },
+    gifts: {
+      title: `Thank you for sharing this very special day with us. 
+        Our most valued gift is your presence with us, but if you'd like to help us start our lives, we will be forever grateful.`,
+      int_transfer:
+        "If you are making an international transfer, you will also need the following",
+      int_transfer_details: `
+          COUNTRY: Spain
+          Currency: Euro
+          Swift/BIC Code (Openbank): OPENESMM
+          ABA Number (Openbank): 0073
+          `,
+    },
+    contact: {
+      title: `
+        If you have any doubts feel free to reach out.
+        We'd love to help out!
+        `,
+    },
+  },
+};
---
+++ src/constants/i18n-resources/es.ts
@@ -0,0 +1,75 @@
+export default {
+  translation: {
+    common: {
+      view: "ver",
+      rsvp: "Confirmar asistencia",
+    },
+    menu: {
+      home: "Info",
+      ceremony: "Iglesia",
+      celebration: "Finca",
+      hotels: "Alojamiento",
+      buses: "Autobuses",
+      taxis: "Taxis",
+      gifts: "Regalos",
+      contact: "Contacto",
+    },
+    home: {
+      title: "¡Nos casamos!",
+      body: "¡Estamos super felices y nos hace mucha ilusión que nos acompañéis en un día tan especial!",
+      info: `
+        <strong>Fecha</strong>: viernes 5 de julio de 2024.
+        <strong>Hora y lugar de la ceremonia</strong>: 17:00pm en la Iglesia de Nuestra Señora de la Asunción (<a style="text-decoration:underline;" rel="noreferrer" target="_blank" href="https://maps.app.goo.gl/moae4YPy4ybuS2tb7">calle verdes 145, 39160, Galizano, Cantabria</a>).
+        <strong>Lugar de celebración</strong>: Finca de San Juan Hostería & Catering (<a style="text-decoration:underline;" rel="noreferrer" target="_blank" href="https://maps.app.goo.gl/ASTmheeSVEdpWMuu8">Barrio de San Juan, s/n, 39660 Villabanez, Cantabria</a>).
+        
+        Mientras llega el gran día, hemos creado esta página web para que podáis tener toda la información que necesitáis.
+        Confirmadnos lo antes posible por favor, que así organizarlo todo nos será mucho más fácil.
+        
+        ¡Nos vemos muy pronto!
+        Cristina & Ricardo`,
+      hotel_rec: "Nuestras recomendaciones de alojamiento",
+    },
+    church: {
+      title: `Iglesia`,
+      location: "Localización",
+    },
+    celebration: {
+      title: `Finca`,
+      location: "Localización",
+    },
+    hotels: {
+      title: "Alojamiento",
+      zone1: "Zona 1: Alrededores de la finca",
+      zone2: "Zona 2: En la ciudad",
+      zone3: "Zona 3: Alrededores de la iglesia",
+    },
+    buses: {
+      title: "Autobuses",
+      body: `
+        Ofreceremos un servicio de autobuses que os llevarán primero a la Iglesia de Nuestra Señora de la Asunción (calle verdes 145, 39160, Galizano, Cantabria), y luego a la Finca de San Juan (barrio de San Juan, s/n, 39660 Villabanez, Cantabria). Una vez terminada la celebración, habrá autobuses de vuelta.
+
+        En las próximas semanas publicaremos el horario y las paradas del servicio de autobuses.`,
+    },
+    taxis: {
+      title:
+        "Hemos recopilado una lista de servicios de taxi de la zona para su comodidad.",
+    },
+    gifts: {
+      title: `Gracias por estar a nuestro lado en un día tan importante para nosotros. 
+        Nuestro mejor regalo es que podáis venir, aún así, si queréis ayudarnos a empezar nuestra vida juntos, os lo agradeceremos siempre.`,
+      int_transfer:
+        "Si vas a realizar una transferencia internacional, también necesitarás esto",
+      int_transfer_details: `
+          PAÍS: España
+          MONEDA: Euro
+          CÓDIGO SWIFT/BIC (Openbank): OPENESMM
+          NÚMERO ABA (Openbank): 0073
+          `,
+    },
+    contact: {
+      title: `
+        Si tienes cualquier duda, no dudes en contactar con nosotros.
+        Estaremos encantados de ayudarte en todo lo que necesites.`,
+    },
+  },
+};
