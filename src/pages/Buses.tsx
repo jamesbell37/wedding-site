e2634012eac6137cbad00137f8d2dec60011986d
Cperezsala <99407104+Cperezsala@users.noreply.github.com>
2024-03-14 20:02:40 +0100

    Fixed issue with map height

--- src/pages/Buses.tsx
+++ src/pages/Buses.tsx
@@ -6,12 +6,12 @@ const Buses = ({ isMenuOpen }: { isMenuOpen: boolean }) => {
   const { t } = useTranslation();
   return (
     !isMenuOpen && (
-      <div className="flex flex-col flex-1 px-5 whitespace-pre-line bg-off-white w-full">
-        <div className="flex flex-col text-2xl pt-5 font-bold">{t("buses.title")}</div>
+      <div className="flex flex-col flex-1 px-5 whitespace-pre-line bg-off-white">
+        <div className="text-2xl pt-5 font-bold">{t("buses.title")}</div>
         <span>{t("buses.body")}</span>
         <div className="overflow-auto">
           <img
-            className="min-w-[25rem] object-scale-down h-fit w-[30rem] rounded-2xl"
+            className="min-w-[25rem] xs:min-w-0 object-scale-down w-[30rem] rounded-2xl"
             src={Image}
          />
         </div>
